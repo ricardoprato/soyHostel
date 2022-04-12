36c320bf5fb7c4376cd5af21b4935ff131f53e67
Luciano Durietz <luciano.durietz@gmail.com>
2022-04-12 18:30:59 -0300

    RoomCard: importe styles de RoomCard.module.css y corregi classnames

--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import GlobalContext from "../../GlobalContext/GlobalContext";
+import styles from './RoomCars.module.css';
 
 export default function RoomCard(props) {
-
   const { cart, setCart } = useContext(GlobalContext);
   const { filterDates } = useContext(GlobalContext);
 
@@ -13,44 +13,45 @@ export default function RoomCard(props) {
     checkOut: filterDates.checkOut,
     roomId: props.roomId,
     bedPrice: props.bedPrice,
-    numberOfBeds: 0
-  })
+    numberOfBeds: 0,
+  });
 
   const onclickHandler = function (arg) {
     if (arg === "+" && count > 0) {
       setCount(count--);
       setToCart({
         ...toCart,
-        numberOfBeds: toCart.numberOfBeds + 1
-      }) 
-    }else if (arg === "-" && count < props.bedsAvailable) {
+        numberOfBeds: toCart.numberOfBeds + 1,
+      });
+    } else if (arg === "-" && count < props.bedsAvailable) {
       setCount(count++);
       setToCart({
         ...toCart,
-        numberOfBeds: toCart.numberOfBeds - 1
-      }) 
-    }else if(arg === "add"){
-      if(props.private){
+        numberOfBeds: toCart.numberOfBeds - 1,
+      });
+    } else if (arg === "add") {
+      if (props.private) {
         setToCart({
           ...toCart,
-          numberOfBeds: 1
-        }) 
+          numberOfBeds: 1,
+        });
       }
-      setCart([...cart, toCart])
+      setCart([...cart, toCart]);
     }
   };
   return (
-    <div className="RoomCardContainer">
+    <div className={styles.RoomCardContainer}>
       <Link to={`/details/${props.roomId}`} /* poner el path correcto */>
         <img
-          className="RoomCardImg"
+          className={styles.RoomCardImg}
           src={`../../img/rooms/smallRoom${props.roomId}.png`}
           alt="room-img"
         />
-        {/* la imagen la vamos a asociar con el ID de habitacion sacandola de /img */}
       </Link>
       <div>Room: {props.name}</div>
-      <div>Availability for {filterDates.checkIn} to {filterDates.checkOut}</div>
+      <div>
+        Availability for {filterDates.checkIn} to {filterDates.checkOut}
+      </div>
       <span>Room description: {props.description}</span>
       <div>
         {props.private ? (
@@ -64,7 +65,9 @@ export default function RoomCard(props) {
             <div>Bed price: $ {props.bedPrice}</div>
             <button onClick={() => onclickHandler("+")}> + </button>
             <button onClick={() => onclickHandler("-")}> - </button>
-            <div>{toCart.numberOfBeds} beds selected, {count} Beds left</div>
+            <div>
+              {toCart.numberOfBeds} beds selected, {count} Beds left
+            </div>
             <button onClick={() => onclickHandler("add")}> ADD to Cart </button>
           </div>
         )}
