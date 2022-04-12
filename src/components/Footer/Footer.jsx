8ba4297d7e2c7f39c849a68584ce212e1c7c12e7
Luciano Leyria <luciano.leyria96@gmail.com>
2022-04-12 14:34:15 -0300

    Footer terminado. falta detallar mas adelante

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,24 @@ import "../Footer/Footer.css";
 
 const Footer = () => {
   return (
-    <div className="container">
+    <footer className="container">
       <iframe
         className="mapa"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1886.5555418056924!2d-57.019182085077766!3d-37.32366949064503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959b5dd0e82128d5%3A0xd9877fa09d587e9a!2sCaba%C3%B1a%20Willow!5e0!3m2!1ses-419!2sar!4v1649704101986!5m2!1ses-419!2sar"
       ></iframe>
-      <h1 className="title">Seguinos en nuestras redes sociales !</h1>
-      <div></div>
-    </div>
+      <div className="icons">
+        <h1 className="title">Seguinos en nuestras redes sociales !</h1>
+        <a href="www.instagram.com">
+          <i type="button" className="bi bi-instagram"></i>
+        </a>
+        <a href="www.facebook.com">
+          <i type="button" className="bi bi-facebook"></i>
+        </a>
+        <a href="www.whatsapp.com">
+          <i type="button" className="bi bi-whatsapp"></i>
+        </a>
+      </div>
+    </footer>
   );
 };
 
