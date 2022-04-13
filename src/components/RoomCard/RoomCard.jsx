316e8b9f7ffde26735592856f12dbb21b4a3c8f1
Luciano Durietz <luciano.durietz@gmail.com>
2022-04-13 14:20:49 -0300

    minima correccion en roomCard solo una letra--- y un poco de avance en RoomDetails

--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import GlobalContext from "../../GlobalContext/GlobalContext";
-import styles from "./RoomCard.modules.css";
+import styles from "./RoomCard.modules.css"; 
 
 export default function RoomCard(props) {
   const { cart, setCart } = useContext(GlobalContext);
