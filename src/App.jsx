d79b3753771e876e6825095cd7e1636f3ea16ad2
Luciano Leyria <luciano.leyria96@gmail.com>
2022-04-13 11:41:58 -0300

    commit pequeños cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <NavBar />
       <Footer />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalContext } from "./components/GlobalContext/GlobalContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <GlobalContext.Provider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </GlobalContext.Provider>,
+  document.getElementById("root")
+);
