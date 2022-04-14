79345e720c8b812480047630ef15738af64e3d19
Luciano Durietz <luciano.durietz@gmail.com>
2022-04-14 14:17:59 -0300

    RoomCard, RoomDetails: bastante trabajo en ambas y un poco de CSS

--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -1,8 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GlobalContext } from '../../GlobalContext/GlobalContext';
-import styles from './RoomCard.modules.css';
-
+import styles from './RoomCard.module.css';
 
 export default function RoomCard(props) {
   const { cart, setCart } = useContext(GlobalContext);
@@ -16,18 +15,21 @@ export default function RoomCard(props) {
     bedPrice: props.bedPrice,
     numberOfBeds: 0,
   };
-  let [count, setCount] = useState(props.bedsAvailable);
+  let countInitialState = props.bedsAvailable
+  let [count, setCount] = useState(countInitialState);
   let [toCart, setToCart] = useState(initialstate);
 
   const oncliCkHandler = function (arg) {
     if (arg === '+' && count > 0) {
-      setCount(count--);
+      let aux = count -1
+      setCount(aux);
       setToCart({
         ...toCart,
         numberOfBeds: toCart.numberOfBeds + 1,
       });
     } else if (arg === '-' && count < props.bedsAvailable) {
-      setCount(count++);
+      let aux = count + 1
+      setCount(aux);
       setToCart({
         ...toCart,
         numberOfBeds: toCart.numberOfBeds - 1,
@@ -45,43 +47,53 @@ export default function RoomCard(props) {
       }
     }
   };
-  console.log(filterDates);
+  console.log(cart);
   return (
     <div className={styles.RoomCardContainer}>
-      {/* <Link to={`/details/${props.roomId}`} >
-        <img
-          className={styles.RoomCardImg}
-          src={`../../img/rooms/smallRoom${props.roomId}.png`}
-          alt="room-img"
-        />
-      </Link> */}
-      <div>Room: {props.name}</div>
-      <div>
-        {/* Availability for {filterDates.checkIn} to {filterDates.checkOut} */}
+      <div className={styles.RoomCardImages}>
+        <Link to={`/details/${props.roomId}`}>
+          <img
+            className={styles.RoomCardImg}
+            src={`https://handyhostel.com/wp-content/uploads/2020/10/nomads-brisbane-hostel-dorm1574757593.jpg`}
+            alt="room-img"
+          />
+        </Link>
       </div>
-      <span>Room description: {props.description}</span>
-      <div>
-        {props.private ? ( // si la habitacion es privada no se agregan + o - camas sino que se reserva la habitacion entera
-          <div>
-            <div>This is a PRIVATE room</div>
-            {props.bathroom ? <div>With private bathroom</div> : null}
-            <div>Room price: $ {props.bedPrice * props.bedsAvailable}</div>
-            <div>Room for {props.bedsAvailable} people</div>
-            <button onClick={() => oncliCkHandler('add')}> ADD to Cart </button>
-          </div>
-        ) : (
-          <div>
-            <div>This is a SHARED room</div>
-            {props.bathroom ? <div>With private bathroom</div> : null}
-            <div>Bed price: $ {props.bedPrice}</div>
-            <button onClick={() => oncliCkHandler('+')}> + </button>
-            <button onClick={() => oncliCkHandler('-')}> - </button>
+      <div className={styles.RoomCardText}>
+        <div>Room: {props.roomName}</div>
+        <div>
+          Availability for {filterDates.checkIn} to {filterDates.checkOut}
+        </div>
+        <span>Room description: {props.description}</span>
+        <div>
+          {props.private ? ( // si la habitacion es privada no se agregan + o - camas sino que se reserva la habitacion entera
             <div>
-              {toCart.numberOfBeds} beds selected, {count} Beds left
+              <div>This is a PRIVATE room</div>
+              {props.bathroom ? <div>With private bathroom</div> : null}
+              <div>Room price: $ {props.bedPrice * props.bedsAvailable}</div>
+              <div>Room for {props.bedsAvailable} people</div>
+              <button onClick={() => oncliCkHandler('add')}>
+                {' '}
+                ADD to Cart{' '}
+              </button>
             </div>
-            <button onClick={() => oncliCkHandler('add')}> ADD to Cart </button>
-          </div>
-        )}
+          ) : (
+            <div>
+              <div>This is a SHARED room</div>
+              {props.bathroom ? <div>With private bathroom</div> : null}
+              <div>Bed price: $ {props.bedPrice}</div>
+              <button onClick={() => oncliCkHandler('+')}> + </button>
+              <button onClick={() => oncliCkHandler('-')}> - </button>
+              <div>
+                {toCart.numberOfBeds} beds selected, {count} Beds left
+              </div>
+              <button onClick={() => oncliCkHandler('add')}>
+                {' '}
+                ADD to Cart{' '}
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
