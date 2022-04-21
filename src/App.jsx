f498a27c2d10cb5415aa7b31c3292a22b6d17921
Luciano Leyria <luciano.leyria96@gmail.com>
2022-04-21 14:00:57 -0300

    commit bastante groso

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
 import Prueba from './pages/Prueba/Prueba';
 import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import NewPassword from './components/NewPassword/NewPassword';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/reserva" element={<Prueba />} />
           <Route path="/signup" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/changepassword" element={<NewPassword />} />
         </Route>
       </Routes>
     </BrowserRouter>
