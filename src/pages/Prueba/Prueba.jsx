5f64b374286ef51375ada1e80f003c83eafd11b6
Ariel Arzamendia <arzamendiaariel@gmail.com>
2022-04-24 18:32:15 -0300

    prueba corregido y global context corregido

--- src/pages/Prueba/Prueba.jsx
+++ src/pages/Prueba/Prueba.jsx
@@ -1,25 +1,7 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
-<<<<<<< HEAD
-import Reserva from '../../components/Reserva/Reserva';
-import Cart from '../../components/Cart/Cart';
-import ListDeleteRoom from '../../components/ListDeleteRoom/ListDeleteRoom';
-
-const Prueba = () => {
-  return <><Reserva/></>;
-=======
-import Register from '../../components/Register/Register';
-import styles from '../../components/Register/Register.module.css';
-import Login from '../../components/Login/Login';
-import Google from '../../components/Google/Google';
-import Reserva from '../Reserva/Reserva';
-import FilterBar from '../../components/FilterBar/FilterBar';
-import Booking from '../../components/Booking/Booking';
-import CreateRoom from '../../components/CreateRoom/CreateRoom';
 
 import Calendar from '../../components/Calendar/Calendar';
-
-import PopupChangePw from '../../components/PopupChangePw/PopupChangePw';
 
 const Prueba = () => {
   return (
@@ -29,7 +11,6 @@ const Prueba = () => {
       </div>
     </>
   );
->>>>>>> cb37293560b5781ee3a3a22965c5cbe84f4a7680
 };
 
 export default Prueba;
