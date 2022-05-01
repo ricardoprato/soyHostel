624bcb4830756ff427e6dbf751a4b220b14a72eb
Luciano Leyria <luciano.leyria96@gmail.com>
2022-05-01 11:34:54 -0300

    push para los  pibes

--- src/components/RoomsAdmin/EditRoom.jsx
+++ src/components/RoomsAdmin/EditRoom.jsx
@@ -1,21 +1,21 @@
 import React, { useContext, useEffect, useState } from 'react';
 
 export default function EditRoom(props) {
-  const handleEditClick = () => {
-    console.log('props en EditRoom--> ', props);
-  };
-  console.log('props name--> ', props.props.nombre);
+  // const handleEditClick = () => {
+  //   console.log('props en EditRoom--> ', props);
+  // };
+  // console.log('props name--> ', props.props.nombre);
 
   return (
     <div>
-      <div key={props.id}>
+      {/* <div key={props.id}>
         <p>{props.props.nombre}</p>
         <p>{props.props.cantCamas}</p>
         <p>{props.props.comodidades}</p>
         <p>{props.props.descripcion}</p>
         <p>{props.props.precio}</p>
         <button onClick={() => handleEditClick(props.id)}>Edit</button>
-      </div>
+      </div> */}
     </div>
   );
 }
